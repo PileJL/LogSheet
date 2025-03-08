287edf0e5a452c2e1e074338668b82df189f24a2
PileJL <pilejlc@gmail.com>
2025-03-08 22:57:47 +0800

    proj upload

---
+++ LogSheet/app/src/main/java/com/example/logsheet/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.logsheet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.logsheet.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.signupText.setOnClickListener(v -> {
+            finish();
+            startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+        });
+    }
+}
